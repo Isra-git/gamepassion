2e30e8199ca1411cbab67c75cc85058763ad85b1
Isra-git <villar_80@hotmail.com>
2025-11-11 21:47:13 +0100

    Refactor: eliminar archivos obsoletos y añadir nuevas carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import Header from "./components/Header";
+import Juego from "./components/Juego";
+import {db} from "./data/db";
 
 function App() {
-  const [count, setCount] = useState(0)
+    
+    //Definimos el estado como un arreglo de covers db
+    const [data, setData] = useState(db)
 
+    //Definimos un estado para el carrito
+    const [cart, setCart] = useState([])
+
+    //Agrega elementos al carrito 
+    function addToCart(item){
+
+        //Comprobamos si el elemento existe en el carrito (no existe:-1)
+        const itemExists = cart.findIndex((game => game.id === item.id))
+
+        //Si existe, realizamos una copia (el state no se puede mutar), incrementamos la cantidad
+        // y lo añadimos al state (cart) original del carro
+        if (itemExists >=0) {          
+           const updatedCart = [...cart]
+           updatedCart[itemExists] = {
+             ...updatedCart[itemExists],
+            quantity: updatedCart[itemExists].quantity +1
+        };
+        setCart(updatedCart);
+        //    updatedCart[itemExists].quantity++
+        //    setCart(updatedCart)
+            
+        }else{
+            const newItem = { ...item, quantity: 1 };
+            setCart([...cart, newItem]);
+        // //Inicializamos el contador de articulos para el carrito
+        // item.quantity = 1
+        // //Si no existe lo añadimos
+        //  setCart([...cart, item])
+         }
+
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React+Israel</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Header 
+        cart = {cart}
+        addToCart = {addToCart}
+
+    />
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((game)=> {
+            return (
+                <Juego
+                    key = {game.id}
+                    game = {game} 
+                    cart = {cart}
+                    setCart= {setCart}
+                    addToCart = {addToCart}
+                />
+            )
+          })}
+          
+          
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GamePassion - Tu tienda de Videojuegos retro- Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,105 @@
+// Importaciones
+import { useMemo } from "react";
+
+// Componente de encabezdo
+export default function Header ({cart,addToCart}){
+
+        //State derivado
+    //Carrito vacio?
+    const isEmpty = useMemo(()=> cart.length === 0,[cart])
+    // Calcula el total del carrito
+   const carTotal = useMemo(() =>
+    cart.reduce((acc, game) => acc + game.price * game.quantity, 0)
+    .toFixed(2),[cart]);
+
+return(
+             
+    <header className="py-5 header">
+        <div className="container-xl">
+            <div className="row justify-content-center justify-content-md-between">
+                <div className="col-8 col-md-3">
+                    <a href="index.html">
+                        <img className="img-fluid logotipo" src="./public/img/logo.png" alt="imagen logo" />
+                    </a>
+                </div>
+                <nav className="col-4 col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div className="carrito">
+                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+
+                        <div id="carrito" className="bg-white p-3">
+                            {isEmpty ?(
+                                 <p className="text-center">El carrito esta vacio</p>
+                            ):( 
+                                <>                                                  
+                                <table className="w-100 table">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {cart.map(game => (                                 
+                                            <tr key={game.id}>
+                                                <td>
+                                                    <img className="img-fluid"  
+                                                    src={`./img/covers/${game.image}`} 
+                                                    alt="imagen del juego" />
+                                                </td>
+                                                <td>{game.name}</td>
+                                                <td className="fw-bold">
+                                                        ${game.price}
+                                                </td>
+                                                <td className="flex align-items-start gap-4">
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-dark"
+                                                    >
+                                                        -
+                                                    </button>
+
+                                                        {game.quantity}
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-dark"
+                                                        onClick={() => addToCart(game)}
+
+                                                    >
+                                                        +
+                                                    </button>
+                                                </td>
+                                                <td>
+                                                    <button
+                                                        className="btn btn-danger"
+                                                        type="button"
+                                                    >
+                                                        X
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            
+                                <p className="text-end">Total pagar: 
+                                <span className="fw-bold">{carTotal} €
+                                </span>
+                                </p>
+                            
+                            </>
+                            )}
+                            <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                                        
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    )
+}
+
